704be61142d1ec212f2da204202e9a6e9feafadb
Unknown <beckettmorsch@gmail.com>
2018-09-01 14:56:23 -0400

    patient table set up
    still need the code to access it

--- TheDatabase/Datafile.sql
+++ TheDatabase/Datafile.sql
@@ -1,8 +1,8 @@
 ﻿-- Script Date: 9/1/2018 2:42 PM  - ErikEJ.SqlCeScripting version 3.5.2.75
 CREATE TABLE [Patients] (
   [PatientID] INTEGER NOT NULL
-, [FirstName] TEXT NULL
+, [FirstName] TEXT NOT NULL
 , [LastName] TEXT NOT NULL
-, [Gender] TEXT NOT NULL
+, [Gender] TEXT NULL
 , CONSTRAINT [PK_Patients] PRIMARY KEY ([PatientID])
 );
